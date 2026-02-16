5acef5e37a14981a4fc5067c89ec029560b0f472
Ekaterina Petrova <peachfly15@gmail.com>
2026-02-16 23:32:33 +0800

    edit main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 import std;
 
-int main(){
-	std::println("hello worrld!");
+int main()
+{
+	std::println("----------------------------------------");
+	std::println("ЛР №: 1");
+	std::println("Группа: 6113");
+	std::println("Автор: Петрова Е.В.");
+	std::println("Вариант: 17");
+	std::println("----------------------------------------");
 }
